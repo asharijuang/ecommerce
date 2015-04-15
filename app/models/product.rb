fed07c97009843eadf4c4d66f9c6f4e90a51cbc0
asharijuang <asharijuang@me.com>
2015-04-15 22:43:54 +0700

    create product validation

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,14 @@
 class Product < ActiveRecord::Base
+	# validation require title,description, and image field
+	validates :title, :description, :image_url, presence: true
+	# validation input price only number
+	validates :price, numericality: {greater_than_or_equal_to: 0.01}
+	# title must uniq or not equal to each other 
+	validates :title, uniqueness: true
+	# validation for image type
+	validates :image_url, allow_blank: true, format: {
+		with: %r{\.(gif|jpg|png)\Z}i,
+		message: 'must be a url for GIF, JPG, or PNG image'
+	}
+
 end
